d3dd2db139272f361ed5588df378e189c5880eb7
MintzyG <ericbraga2000@gmail.com>
2023-10-03 21:07:52 -0300

    Quicksort Visualization now works on any console size

--- Rivera/Sorting-Algs/Quicksort/VectorUtility.c
+++ Rivera/Sorting-Algs/Quicksort/VectorUtility.c
@@ -3,46 +3,42 @@
 
 #ifdef DEBUG
 
-void PopulateVectorDebug(int* Vector, int Size, int Line) {
-    int Amount = 0;
-    for (int i = 0; i < Size; i++) {
-        Vector[i] = rand() % 56;  
-        Amount++;
-    }  
-    printf("\nDEBUG -- PopulateVector | [INSERTED %d VALUES AT LINE %d] -- DEBUG\n", Amount, Line);
-}
-
-int* CreateVectorDebug(int Size, int Line) {
-    int* Vector;
-    Vector = (int*)malloc(Size * sizeof(int));
-    printf("\nDEBUG -- [VECTOR ALLOCATED AT LINE %d] -- DEBUG\n", Line);
-    return Vector;
-}
+    void PopulateVectorDebug(int* vector, int size, int max_value, int line) {
+        int amount = 0;
+        for (int i = 0; i < size; i++) {
+            vector[i] = rand() % max_value;  
+            amount++;
+        }  
+        printf("\nDEBUG -- PopulateVector | [INSERTED %d VALUES AT LINE %d] -- DEBUG\n", amount, line);
+    }
+    
+    int* CreateVectorDebug(int size, int line) {
+        int* vector;
+        vector = (int*)malloc(size * sizeof(int));
+        printf("\nDEBUG -- [VECTOR ALLOCATED AT LINE %d] -- DEBUG\n", line);
+        return vector;
+    }
 
 #endif
 
-void ShowVector(int* Vector, int Size, int width, int block_size) {
-    printf("\n");
+void ShowVector(int* vector, int size) {
+    for(int i = 0; i < size; i++)
+        printf("%d ", vector[i]);
+} 
 
-    printf(" %*s", (block_size + 1)/2, " ");
-    for (int i = 0; i < Size; i++) {
-        printf("%d%*s", Vector[i], block_size-1, " ");
-    }
+void PopulateVector(int* vector, int size, int max_value) {
+    for (int i = 0; i < size; i++)
+        vector[i] = rand() % max_value;  
 }
 
-void PopulateVector(int* Vector, int Size) {
-    for (int i = 0; i < Size; i++)
-        Vector[i] = rand() % 56;  
-}
-
-int* CreateVector(int Size) {
-    int* Vector;
-    Vector = (int*)malloc(Size * sizeof(int));
-    return Vector;
+int* CreateVector(int size) {
+    int* vector;
+    vector = (int*)malloc(size * sizeof(int));
+    return vector;
 }
 
 #ifdef DEBUG
-    #define PopulateVector(A, B) PopulateVectorDebug(A, B, __LINE__)
+    #define PopulateVector(A, B, C) PopulateVectorDebug(A, B, C, __LINE__)
     #define CreateVector(A) CreateVectorDebug(A, __LINE__)
 #endif
 
