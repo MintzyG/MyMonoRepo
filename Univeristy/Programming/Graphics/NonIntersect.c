04102e15d524c4f153256a9516b270f6c6ee2de0
MintzyG <ericbraga2000@gmail.com>
2023-10-20 19:48:40 -0300

    Coded nonIntersect, and helped emanuel's debug implementation

--- Univeristy/Programming/Graphics/NonIntersect.c
+++ Univeristy/Programming/Graphics/NonIntersect.c
@@ -14,9 +14,11 @@
 
 // CosTheta = Esc(A,B)/|A|*|B|
 
+#define RAND 200
+
 typedef struct Ponto {
     float x, y;
-    float ang, dir;
+    float ang, dir, dist;
 } Ponto;
 
 void ShowPoints(Ponto* ponto, int size){
@@ -29,16 +31,22 @@ void ShowPoints(Ponto* ponto, int size){
     printf("\n");
 }
 
+Ponto CreatePoint(Ponto ponto){
+    int sign1 = 0, sign2 = 0;
+    sign1 = rand() % 2;
+    if (sign1 == 0) 
+        sign1 = -1;
+    sign2 = rand() % 2;
+    if (sign2 == 0) 
+        sign2 = -1;
+    ponto.x = sign1 * ((rand() % RAND) + 1); 
+    ponto.y = sign2 * ((rand() % RAND) + 1);
+    return ponto;
+}
+
 void CreatePoints(Ponto* ponto, int size){
-    int sign1, sign2;
-    for (int i = 0; i < size; i++){ 
-        sign1 = rand() % 2;
-        if (sign1 == 0) sign1 = -1;
-        sign2 = rand() % 2;
-        if (sign2 == 0) sign2 = -1;
-        ponto[i].x = sign1 * rand() % 200; 
-        ponto[i].y = sign2 * rand() % 200; 
-    }
+    int exists = 0;
+    for (int i = 0; i < size; i++) ponto[i] = CreatePoint(ponto[i]);
 }
 
 void Init(Ponto* ponto){
@@ -48,25 +56,62 @@ void Init(Ponto* ponto){
     ponto->dir = 1;
 }
 
-/*
 void PrintPonto(Ponto ponto){
-    printf("X: %f\nY: %f\nAng: %f\nDir: %f", 
+    printf("X: %f\tY: %f\tAng: %f\tDir: %f\n", 
             ponto.x,
             ponto.y,
             ponto.ang,
             ponto.dir);
 }
-*/
+
+void Direcao(Ponto* ponto, int size){
+    for (int i = 1; i < size; i++) {
+        float dir = ponto[0].x * ponto[i].y - ponto[0].y * ponto[i].x;
+        if (dir != 0){
+            dir = dir / fabs(dir);
+            ponto[i].dir = dir;
+        } else
+            ponto[i].dir = 0;
+    }
+}
 
 void CalcularAngulos(Ponto* ponto, int size){
     for (int i = 1; i < size; i++) {
-        float produto_escalar = ponto[1].x * ponto[i].x +
-                                ponto[1].y * ponto[i].y;
-        float distancia_um = sqrt(pow(ponto[1].x, 2) + pow(ponto[1].y, 2));
+        float produto_escalar = ponto[0].x * ponto[i].x + ponto[0].y * ponto[i].y;
+        float distancia_um = sqrt(pow(ponto[0].x, 2) + pow(ponto[0].y, 2));
         float distancia_dois = sqrt(pow(ponto[i].x, 2) + pow(ponto[i].y, 2));
-        float cosseno_radianos = acos(produto_escalar / (distancia_um * distancia_dois));
-        ponto[i].ang = cosseno_radianos * 180.0 / M_PI;
+        float proj = produto_escalar / (distancia_um * distancia_dois);
+        printf("\n%f", proj);
+        if (proj < -1) proj = 0;
+        if (proj > 1) proj = 1;
+        float cosseno_radianos = acos((proj));
+        float ang = cosseno_radianos * 180.0 / M_PI;
+        ponto[i].ang = ang;
 
+        printf("\nPonto (%f,%f): PE:%f D1:%f d2:%f PJ:%f CR:%f A:%f", ponto[i].x, ponto[i].y, produto_escalar, distancia_um, distancia_dois, proj, cosseno_radianos, ang);
+
+    }
+}
+
+void ConsertaSimilar(Ponto* ponto, int size) {
+    int fabs1, fabs2, fabr1, fabr2;
+    fabr1 = fabs(ponto[0].x);
+    fabr2 = fabs(ponto[0].y);
+    PrintPonto(ponto[0]);
+    for (int i = 1; i < size; i++){
+        fabs1 = fabs(ponto[i].x);
+        fabs2 = fabs(ponto[i].y);
+        if (fabr1 == fabs1 && fabr2 == fabs2){
+            if((ponto[0].x == ponto[i].x) && (ponto[0].y == ponto[i].y))
+                printf("\nWas: ");
+            PrintPonto(ponto[i]);
+            if((ponto[0].x == ponto[i].x) && (ponto[0].y == ponto[i].y)){
+                printf("Now: ");
+                ponto[i] = CreatePoint(ponto[i]);
+                PrintPonto(ponto[i]);
+                printf("\n");
+            }
+        }
     }
 }
 
@@ -86,7 +131,9 @@ int main() {
     CreatePoints(vetor_pontos, size);
     printf("Your points: \n");
     ShowPoints(vetor_pontos, size);
+    ConsertaSimilar(vetor_pontos, size);
     CalcularAngulos(vetor_pontos, size);
+    Direcao(vetor_pontos, size);
     ShowPoints(vetor_pontos, size);
     return 0;
 }
