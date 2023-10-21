015b54b0b73ecb339c25446fb8614cb0afa2c5a3
MintzyG <ericbraga2000@gmail.com>
2023-10-21 12:32:58 -0300

    Update nonIntersect to generate floats

--- Univeristy/Programming/Graphics/NonIntersect.c
+++ Univeristy/Programming/Graphics/NonIntersect.c
@@ -23,29 +23,35 @@ typedef struct Ponto {
 
 void ShowPoints(Ponto* ponto, int size){
     for (int i = 0; i < size; i++){
-        printf("%7.2f\t", ponto[i].x);
-        printf("%7.2f\t", ponto[i].y);
-        printf("%7.2f\t", ponto[i].ang);
-        printf("%7.2f\n", ponto[i].dir);
+        printf("X:%7.2f\t", ponto[i].x);
+        printf("Y:%7.2f\t", ponto[i].y);
+        printf("A:%7.2f\t", ponto[i].ang);
+        printf("DR:%7.2f\t", ponto[i].dir);
+        printf("DS:%7.2f\n", ponto[i].dist);
     }
     printf("\n");
 }
 
 Ponto CreatePoint(Ponto ponto){
     int sign1 = 0, sign2 = 0;
-    sign1 = rand() % 2;
-    if (sign1 == 0) 
-        sign1 = -1;
-    sign2 = rand() % 2;
-    if (sign2 == 0) 
-        sign2 = -1;
-    ponto.x = sign1 * ((rand() % RAND) + 1); 
-    ponto.y = sign2 * ((rand() % RAND) + 1);
+    while(1){
+        sign1 = rand() % 2;
+        if (sign1 == 0) 
+            sign1 = -1;
+        sign2 = rand() % 2;
+        if (sign2 == 0) 
+            sign2 = -1;
+        ponto.x = sign1 * (float)(((double)rand()/(double)(RAND_MAX)) * RAND); 
+        ponto.y = sign2 * (float)(((double)rand()/(double)(RAND_MAX)) * RAND); 
+        if (ponto.x == 0 && ponto.y == 0){
+            continue;
+        }
+        break;
+    }
     return ponto;
 }
 
 void CreatePoints(Ponto* ponto, int size){
-    int exists = 0;
     for (int i = 0; i < size; i++) ponto[i] = CreatePoint(ponto[i]);
 }
 
@@ -64,7 +70,7 @@ void PrintPonto(Ponto ponto){
             ponto.dir);
 }
 
-void Direcao(Ponto* ponto, int size){
+void CalcularDirecao(Ponto* ponto, int size){
     for (int i = 1; i < size; i++) {
         float dir = ponto[0].x * ponto[i].y - ponto[0].y * ponto[i].x;
         if (dir != 0){
@@ -81,15 +87,13 @@ void CalcularAngulos(Ponto* ponto, int size){
         float distancia_um = sqrt(pow(ponto[0].x, 2) + pow(ponto[0].y, 2));
         float distancia_dois = sqrt(pow(ponto[i].x, 2) + pow(ponto[i].y, 2));
         float proj = produto_escalar / (distancia_um * distancia_dois);
-        printf("\n%f", proj);
+        //printf("\n%f", proj);
         if (proj < -1) proj = 0;
         if (proj > 1) proj = 1;
         float cosseno_radianos = acos((proj));
         float ang = cosseno_radianos * 180.0 / M_PI;
         ponto[i].ang = ang;
-
-        printf("\nPonto (%f,%f): PE:%f D1:%f d2:%f PJ:%f CR:%f A:%f", ponto[i].x, ponto[i].y, produto_escalar, distancia_um, distancia_dois, proj, cosseno_radianos, ang);
-
+        //printf("\nPonto (%f,%f): PE:%f D1:%f d2:%f PJ:%f CR:%f A:%f", ponto[i].x, ponto[i].y, produto_escalar, distancia_um, distancia_dois, proj, cosseno_radianos, ang);
     }
 }
 
@@ -97,7 +101,6 @@ void ConsertaSimilar(Ponto* ponto, int size) {
     int fabs1, fabs2, fabr1, fabr2;
     fabr1 = fabs(ponto[0].x);
     fabr2 = fabs(ponto[0].y);
-    PrintPonto(ponto[0]);
     for (int i = 1; i < size; i++){
         fabs1 = fabs(ponto[i].x);
         fabs2 = fabs(ponto[i].y);
@@ -115,12 +118,37 @@ void ConsertaSimilar(Ponto* ponto, int size) {
     }
 }
 
+void AdjustDirection(Ponto* ponto, int size) {
+    for (int i = 0; i < size; i++)
+        if (ponto[i].dir == -1){
+            ponto[i].dir = 1;
+            ponto[i].ang = 360 - ponto[i].ang;
+        }
+}
+
+void SortByAngle(Ponto* ponto, int size) {
+    Ponto tmp;
+    for (int i = 0; i < size; i++)
+        for (int j = 0; j < size - 1; j++){
+            if (ponto[j].ang > ponto[j + 1].ang){
+                tmp = ponto[j + 1];
+                ponto[j + 1] = ponto[j];
+                ponto[j] = tmp;
+            }
+        }
+}
+
+void CalcularDistancia(Ponto* ponto, int size){
+    for (int i = 0; i < size; i++) {
+        ponto[i].dist = sqrt(pow(ponto[i].x, 2) + pow(ponto[i].y, 2));
+    }
+}
+
 int main() {
     srand(time(NULL));
 
     Ponto ponto;
     Init(&ponto);
-    //PrintPonto(ponto);
 
     int size;
 
@@ -132,8 +160,11 @@ int main() {
     printf("Your points: \n");
     ShowPoints(vetor_pontos, size);
     ConsertaSimilar(vetor_pontos, size);
+    CalcularDistancia(vetor_pontos, size);
     CalcularAngulos(vetor_pontos, size);
-    Direcao(vetor_pontos, size);
+    CalcularDirecao(vetor_pontos, size);
+    AdjustDirection(vetor_pontos, size);
+    SortByAngle(vetor_pontos, size);
     ShowPoints(vetor_pontos, size);
     return 0;
 }
